43b33a0eb38adca7e719767fb480c56f9ee05d2e
Jackson Isaac <jacksonisaac2008@gmail.com>
2015-08-24 23:11:41 +0530

    Update portgsoc alias to portlibsolv.

--- mplibsolv.sh
+++ mplibsolv.sh
@@ -13,18 +13,18 @@ mkdir -p ~/Development/libsolv
 cd ~/Development/libsolv
 echo "Fetching gsoc15-dependency branch"
 svn co https://svn.macports.org/repository/macports/branches/gsoc15-dependency
-#PID=$!
+PID=$!
 cd gsoc15-dependency/base
-echo "Installing portgsoc"
+echo "Installing portlibsolv"
 echo "Cleaning PATH so that readline doesn't raise error if MacPorts is already installed"
 env PATH=/usr/bin:/bin:/usr/sbin:/sbin CFLAGS="-pipe -Os" ./configure --enable-readline --prefix ~/Development/libsolv/mp_gsoc && make
 #PID=$!
 sudo make install
 #PID=$!
 
-echo "alias portgsoc=\"$HOME/Development/libsolv/mp_gsoc/bin/port\"" >> ~/.bash_profile
+echo "alias portlibsolv=\"$HOME/Development/libsolv/mp_gsoc/bin/port\"" >> ~/.bash_profile
 echo "alias sudo=\"sudo \"" >> ~/.bash_profile
 
-echo "Usage: portgsoc -l search <portname>\n sudo portgsoc -l install <portname>"
-echo "Setting back the PATH from ~/.bash_profile and sourcing the latest portgsoc aliases"
+echo "Usage: portlibsolv -l search <portname>\n sudo portlibsolv -l install <portname>"
+echo "Setting back the PATH from ~/.bash_profile and sourcing the latest portlibsolv aliases"
 source ~/.bash_profile
